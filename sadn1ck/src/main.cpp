c3aacbf5347fdcb1c56b1a261fb0d766751fa1d8
Anik Das <anikdas0811@gmail.com>
2020-10-03 21:07:10 +0530

    :pencil: Updated init to initialize another folder :bug: Fixed a bug where .imperiumIgnore was written inside .imperium
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- sadn1ck/src/main.cpp
+++ sadn1ck/src/main.cpp
@@ -6,8 +6,10 @@
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <type_traits>
 
 #define INIT "init"
+#define ADD "add"
 
 /// Displaying help if no commands are given
 void displayHelp() {
@@ -15,33 +17,42 @@ void displayHelp() {
   std::cout << "Start a working area\n";
   std::cout
       << "\tinit\t\tInitialise an empty repository inside the current folder\n";
+  std::cout << "\tadd\t\tAdd files to staging area\n";
   std::cout << "\n";
 }
 
 // https://stackoverflow.com/q/18100097
 /// check if directory exists
 bool dirExists(const char *path) {
-  // @TODO: Add error checks
+  // TODO: Add error checks
   struct stat statcheck;
   return stat(path, &statcheck) == 0 && S_ISDIR(statcheck.st_mode);
 }
 
 /// create directory at path
 void createDir(const char *path) {
-  // @TODO: Add error checks
+  // TODO: Add error checks
   // std::cout << "Inside createDir: " << path << "\n";
   mkdir(path, 0777);
 }
 
+/// custom function to write .imperiumIgnore file
+void writeIgnoreFile(const char *ignorePath) {
+  std::ofstream customFileWriter;
+  customFileWriter.open((ignorePath) + std::string("/.imperiumIgnore"));
+  customFileWriter << "node_modules\n.env\n";
+  customFileWriter.close();
+}
+
 /// imperium init function
 void init(const char *path) {
-  // @TODO: Add error checks
-  // std::cout << "Inside init: " << path << "\n";
+  // TODO: Add error checks
   if (dirExists(path)) {
     std::cout << "Existing imperium repository\n";
   } else {
     createDir(path);
     std::ofstream customFileWriter;
+    // TODO: create a custom function to handle file writes
     customFileWriter.open((path) + std::string("/conflict"));
     customFileWriter << "false\n";
     customFileWriter.close();
@@ -51,6 +62,7 @@ void init(const char *path) {
     customFileWriter.open((path) + std::string("/add.log"));
     customFileWriter << "\n";
     customFileWriter.close();
+
     std::cout << "Initialised empty imperium repository at " << path << "\n";
   }
 }
@@ -59,18 +71,31 @@ int main(int argc, char **argv) {
   if (argc <= 1) {
     displayHelp();
   } else {
-    // @TODO: check if there are 2 or more arguments
     // $ imperium <command> [<args>]
-    // branch from <command>
-    // std::cout << argv[1] << "\n";
+    /// if init is called
     if (strcmp(argv[1], INIT) == 0) {
       if (argc == 2) {
+        const char *ignorePath = std::getenv("PWD");
+        writeIgnoreFile(ignorePath);
         const char *path = strcat(std::getenv("PWD"), "/.imperium");
         init(path);
       }
       if (argc == 3) {
-        // @TODO: Add functionality for creating folder along with this
+        // TODO: Add functionality for creating folder along with this
+        createDir(argv[2]);
+        char *temp = strcat(std::getenv("PWD"), "/");
+        temp = std::strcat(temp, argv[2]);
+        // write .imperiumIgnore here
+        const char *ignorePath = temp;
+        writeIgnoreFile(ignorePath);
+        // written .imperiumIgnore here
+        temp = std::strcat(temp, "/.imperium");
+        const char *path = temp;
+        init(path);
       }
+    }
+    if (strcmp(argv[1], ADD) == 0) {
+      std::cout << "Add called\n";
     }
   }
   return 0;
