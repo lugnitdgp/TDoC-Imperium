b85d633b28393dc492804028fecb103f210dc22b
Soham Pal <sohampal101@gmail.com>
2020-10-04 19:05:33 +0530

    pre final stage of add()

--- soham0-0/src/main.cpp
+++ soham0-0/src/main.cpp
@@ -6,25 +6,31 @@
 #include <cstring>
 
 class imperium {
-    std::string path;
+    std::string root;
+
+    /*
+        Checks if the provided path exists or not, if it returns its type.
+        @param pathArgument passed path 
+    */
+    std::string doesExist(std::string pathArgument);
     public:
     /*
         constructor
-        Sets path to present working directory.
+        Sets root to present working directory.
     */
     imperium();
 
     /*
-        Sets the path for repo directory if provided.
-        @param pathQuery path of the repo directory relative to present working directory.
+        Sets the root for repo directory if provided.
+        @param pathArgument path of the repo directory relative to present working directory.
     */
-    void setPath(std::string pathQuery);
+    void setRoot(std::string pathArgument);
 
-    //  Initializes empty repository at path directory
+    //  Initializes empty repository at root directory
     void init();
 
     //  Adds current state to the staging area
-    void add();
+    void add(std::string pathArgument);
 
     //  Commits the tracked changes
     void commit();
@@ -35,7 +41,7 @@ class imperium {
     //  Reverts back to last commit
     void revert();
 
-    //  Shows status of the 
+    //  Shows status of the tracked items
     void getStatus();
 };
 
@@ -46,64 +52,84 @@ int main(int argc, char **argv){
     }
 
     imperium repository;
-
     if(!strcmp(argv[1], "init")){
-        if(argc==3) repository.setPath(argv[2]);
+        if(argc==3) repository.setRoot(argv[2]);
         repository.init();
+    }
+    else if(!strcmp(argv[1], "add")){
+        int currentArgumentNumber = 0;
+        for(; currentArgumentNumber<argc; currentArgumentNumber++){
+            repository.add(argv[currentArgumentNumber]);
+        }
     }
     return 0;
 }
 
 imperium::imperium(){
     if(getenv("dir")){
-        path = getenv("dir");
+        root = getenv("dir");
     }else{
         std::cout << "No environment variable found!" << std::endl;
         exit(0);
     }
 }
 
-void imperium::setPath(std::string pathQuery){
-    path += "/" + pathQuery;
-    if((mkdir((path).c_str(), 0777))==-1){
+std::string imperium::doesExist(std::string pathArgument){
+    struct stat sb;
+    if(!stat(pathArgument.c_str(), &sb)){
+        if(S_ISDIR(sb.st_mode)){
+            return "directory";
+        }
+        else if(S_ISREG(sb.st_mode)){
+            return "file";
+        }
+    }
+    return "\0";
+}
+
+void imperium::setRoot(std::string pathArgument){
+    root += "/" + pathArgument;
+    if((mkdir((root).c_str(), 0777))==-1){
         std::cerr << "Error: " << strerror(errno) << std::endl;
     } 
 }
 
 void imperium::init(){
-    struct stat sb;
-    if(!stat((path+"/.imperium").c_str(), &sb) && S_ISDIR(sb.st_mode)){
+    if(doesExist(root+"/.imperium")!="\0"){
         std::cout<<"Repository has already been initialized" << std::endl;
         return ;
     }
     
-    if((mkdir((path+"/.imperium").c_str(), 0777))==-1){
+    if((mkdir((root+"/.imperium").c_str(), 0777))==-1){
         std::cerr << "Error: " << strerror(errno) << std::endl;
     }  
     
     std::fstream fileWriter;
-    fileWriter.open ((path+"/.imperiumIgnore").c_str(), std::fstream::out | std::fstream::app);
+    fileWriter.open ((root+"/.imperiumIgnore").c_str(), std::fstream::out | std::fstream::app);
     fileWriter << "/.imperium\n/.imperiumIgnore\n/.node_modules\n/.env\n";
     fileWriter.close(); 
 
-    fileWriter.open ((path+"/.imperium/conflict").c_str(), std::fstream::out | std::fstream::app);
+    fileWriter.open ((root+"/.imperium/conflict").c_str(), std::fstream::out | std::fstream::app);
     fileWriter << "false\n";
     fileWriter.close(); 
     
-    fileWriter.open ((path+"/.imperium/commit.log").c_str(), std::fstream::out | std::fstream::app);
+    fileWriter.open ((root+"/.imperium/commit.log").c_str(), std::fstream::out | std::fstream::app);
     fileWriter << "";
     fileWriter.close(); 
 
-    fileWriter.open ((path+"/.imperium/add.log").c_str(), std::fstream::out | std::fstream::app);
+    fileWriter.open ((root+"/.imperium/add.log").c_str(), std::fstream::out | std::fstream::app);
     fileWriter << "";
     fileWriter.close();
     
-    std::cout << "Initialized empty imperium repository at " << path << std::endl;
+    std::cout << "Initialized empty imperium repository at " << root << std::endl;
     return ;
 }
 
-void imperium::add(){
-    //  to do
+void imperium::add(std::string pathArgument){
+    if(pathArgument == "."){
+        pathArgument = "";
+    } 
+    
     return ;
 }
 
