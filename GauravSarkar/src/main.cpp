d26633edcfc253b131ff7bc64655a8d267a61cfa
GauravSarkar <60042820+GauravSarkar@users.noreply.github.com>
2020-10-05 18:33:22 +0530

    Update main.cpp

--- GauravSarkar/src/main.cpp
+++ GauravSarkar/src/main.cpp
@@ -1,13 +1,16 @@
-#include <bits/stdc++.h>
 #include <fstream>
-#include <string>
-#include <string.h>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <utility>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <experimental/filesystem>
 
+using namespace std;
+using namespace std::experimental::filesystem;
 
-namespace fs = std::experimental::filesystem;
-std::string root= "";
+string root = "";
 void init(std::string path)
 {
 	struct stat buffer;
@@ -44,26 +47,201 @@ void init(std::string path)
 }
 }
 
-void add(std::string path)
-{
-	//add functionality
-	
-}
-int main(int argc, char const *argv[])
-{
-    
-    const char *Directory = getenv("dir");
+//add function
 
-    if (argc == 1)
-        std::cout << "Hello Welcome to my world of Imperium";
-    else if (argc == 2)
+void addtoCache(const path &currentPath)
+{
+    int len = root.length();
+
+    string relativePathString = currentPath.string().substr(len);
+    
+    path relativePath(relativePathString);
+
+    path rootPath(root);
+    path rootPathInUserDirectory(root);
+
+    rootPath /= ".imperium";
+    rootPath /= ".add";
+
+    for (const auto &cur : relativePath)
     {
-        if (strcmp(argv[1], "init")==0)
-            init(Directory);
-		f (strcmp(argv[1], "add")==0)
-            add(Directory);
-        else if (strcmp(argv[1], "--help")==0)
-            std::cout<<"Help is always provided just look for it.May the source be with you.";
+        if (cur == "/")
+            continue;
+        rootPath /= cur;
+        rootPathInUserDirectory /= cur;
+
+        
+        if (is_directory(rootPathInUserDirectory))
+        {
+            
+            if (!exists(rootPath))
+            {
+                create_directory(rootPath.string().c_str());
+            }
+        }
+        else
+        {
+            
+            const auto copyOptions = copy_options::update_existing;
+            if (!exists(rootPath))
+            {
+                cout << currentPath << " Initialized.\n";
+            }
+            else
+            {
+                cout << currentPath << " Updated.\n";
+            }
+            copy_file(currentPath.string(), rootPath.string(), copyOptions);
+        }
     }
+
+    
+}
+
+bool searchString(ifstream &inputFile, const string &str)
+{
+
+   
+    string line;
+    while (getline(inputFile, line))
+    {
+        if (line.find(str) != string::npos)
+            return true;
+    }
+    return false;
+}
+
+void toBeAdded(const path &currentPath)
+{
+
+    string addLogPath = root + "/.imperium/addlog";
+    ifstream addLogFile(addLogPath.c_str());
+
+    if (!searchString(addLogFile, currentPath.u8string()))
+    {
+        
+        ofstream fout(addLogPath.c_str(), ios::app);
+        fout << currentPath << "\n";
+        fout.close();
+    }
+    else
+    {
+        
+    }
+    addLogFile.close();
+}
+
+bool toBeIgnored(const path &currentPath)
+{
+
+    string ignorePath = root + "/.imperiumIgnore";
+
+    string currentPathString = currentPath.string();
+    string line;
+    ifstream inputFile(ignorePath);
+
+    while (getline(inputFile, line))
+    {
+        string Directory = root + line;
+        if (line.back() == '/')
+        {
+            Directory.pop_back();
+            if (currentPathString.find(Directory) != string::npos)
+            {
+                
+                return true;
+            }
+        }
+        else
+        {
+            
+            if (currentPathString == Directory)
+                return true;
+        }
+    }
+    inputFile.close();
+    return false;
+}
+void addUtil(const path &currentPath)
+{
+
+    string imperiumFolder = root + "/.imperium";
+    string addFolder = imperiumFolder + "/.add";
+
+    if (!toBeIgnored(currentPath))
+    {
+        toBeAdded(currentPath);
+
+        
+        mkdir(addFolder.c_str(), 0777);
+        addtoCache(currentPath);
+    }
+    
+}
+
+void add(vector<string> &argv)
+{
+
+    string imperiumFolder = root + "/.imperium";
+    string addFolder = imperiumFolder + "/.add";
+    string addLogPath = imperiumFolder + "/addlog";
+    string ignorePath = imperiumFolder + "/ignorelog";
+
+    if (!exists(addLogPath))
+    {
+        cout << "Please initialize the repository first.\n";
+        return;
+    }
+
+    for (auto &str : argv)
+    {
+
+        if (str == ".")
+        {
+            str = "";
+        }
+        path currentPath(root);
+        currentPath /= str;
+        if (!exists(currentPath))
+        {
+            cout << "Sorry the file: " << currentPath << " dosen't exist.\n";
+            continue;
+        }
+
+        if (is_regular_file(currentPath))
+        {
+            addUtil(currentPath);
+            continue;
+        }
+        
+        for (auto &p : recursive_directory_iterator(currentPath))
+        {
+           
+            addUtil(p.path());
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    root = getenv("dir");
+ if (argc == 1)
+        std::cout << "Hello Welcome to my world of Imperium";
+    else if (!strcmp(argv[1], "init"))
+    {
+        init(root);
+    }
+    else if (!strcmp(argv[1], "add"))
+    {
+        
+        vector<string> Args;
+        for (int i = 2; i < argc; i++)
+        {
+            Args.push_back(argv[i]);
+        }
+        add(Args);
+    }
+	 else if (strcmp(argv[1], "--help")==0)
+            std::cout<<"Help is always provided just look for it.May the source be with you.";
     return 0;
 }
