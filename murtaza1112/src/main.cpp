7527b2fc45b943e6628a18334fadfc1d92110973
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2020-10-07 12:08:13 +0530

    Added the commit functionality

--- murtaza1112/src/main.cpp
+++ murtaza1112/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -6,8 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <experimental/filesystem>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <openssl/sha.h>
 
 using namespace std;
+namespace fs = std::experimental::filesystem;
 using namespace std::experimental::filesystem;
 
 string root = "";
@@ -26,7 +32,7 @@ void init(string root)
 
     string ignorePath = root + "/.imperiumIgnore";
     ofstream ignore(ignorePath.c_str());
-    ignore << "/.imperium\n/node_modules\n/.env\n.imperiumIgnore"
+    ignore << "/.imperium/\n/node_modules/\n/.env/\n/.imperiumIgnore"
            << "\n";
     ignore.close();
 
@@ -162,6 +168,7 @@ bool toBeIgnored(const path &currentPath)
     {
         string Directory = root + line;
         // cout<<"directory"<<Directory<<"\n";
+
         if (line.back() == '/')
         {
 
@@ -186,6 +193,7 @@ bool toBeIgnored(const path &currentPath)
     inputFile.close();
     return false;
 }
+
 void addUtil(const path &currentPath)
 {
 
@@ -219,7 +227,8 @@ void add(vector<string> &argv)
 
     if (!exists(addLogPath))
     {
-        cout << "Please initialize the repository first.\n";
+        ofstream add(addLogPath.c_str());
+        add.close();
         return;
     }
 
@@ -257,9 +266,163 @@ void add(vector<string> &argv)
     }
 }
 
+//commit functionality
+
+string getCurrentTime()
+{
+    time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    string s(30, '\0');
+    strftime(&s[0], s.size(), "%Y-%m-%d-%H:%M:%S", std::localtime(&now));
+    return s;
+}
+
+void updateCommitLog(string hash, string message)
+{
+    string imperiumPath = root + "/.imperium";
+    string commitLogPath = imperiumPath + "/commitlog";
+    string tempFilePath = imperiumPath + "/tempfile";
+    ifstream in(commitLogPath);
+    ofstream temp(tempFilePath);
+    temp << hash << " -- commit " << message << "\n";
+    temp << in.rdbuf();
+    temp.close();
+    in.close();
+    fs::remove(commitLogPath);
+    std::rename(tempFilePath.c_str(), commitLogPath.c_str());
+}
+
+void getPreviousCommit(string prevHash, string currentHash)
+{
+    //
+    string imperiumPath = root + "/.imperium";
+    string addPath = imperiumPath + "/.add";
+    string prevCommitPath = imperiumPath + "/.commit/" + prevHash;
+    string currentCommitPath = imperiumPath + "/.commit/" + currentHash;
+
+    for (auto &p : recursive_directory_iterator(prevCommitPath))
+    {
+        string relative = p.path().string().substr(prevCommitPath.size());
+        // cout<<relative<<"\n";
+        string currentPath = root + relative;
+        string hashFolderPath = currentCommitPath + relative;
+
+        //check if currentpath exists now in the project
+        if (exists(currentPath))
+        {
+            //if directory then add if not existing already
+            //if file then also add if not existing already
+            if (!exists(hashFolderPath))
+            {
+                if (is_directory(currentPath))
+                    create_directory(hashFolderPath);
+                else
+                {
+                    fs::copy_file(currentPath, hashFolderPath);
+                }
+            }
+        }
+    }
+}
+void commit(vector<string> &args)
+{
+
+    string imperiumPath = root + "/.imperium";
+    string addPath = imperiumPath + "/.add";
+    string commitLogPath = imperiumPath + "/commitlog";
+    string addLogPath = imperiumPath + "/addlog";
+    string message;
+    //firrst check if message passed
+    // cout<<args.size();
+    if (args.size() < 2 || args[0] != "-m")
+    {
+        cout << "Plese add a message with the commit.\n";
+        return;
+    }
+    message = args[1];
+    //next check if imperium exists
+    if (!exists(imperiumPath))
+    {
+        cout << "Please initialize the imperium repository.\n";
+        return;
+    }
+
+    //get time
+    string currentTime = getCurrentTime();
+
+    //hash
+    unsigned char obuf[20];
+    unsigned char time[21];
+    char fintime[41];
+    strcpy((char *)time, currentTime.c_str());
+    SHA1(time, currentTime.size(), obuf);
+    // cout<<currentTime<<"\n";
+
+    for (int i = 0; i < 20; i++)
+    {
+        // printf("%02x",obuf[i]);
+        sprintf(&fintime[i * 2], "%02x", obuf[i]);
+    }
+    printf("\nSHA1 HASH: %s\n", fintime);
+    string commitHash(fintime);
+    // cout<<"\n";
+    // cout<<commitHash<<"\n";
+
+    //now create a folder with .commit
+    string commitPath = imperiumPath + "/.commit";
+    if (!exists(commitPath))
+    {
+        create_directory(commitPath);
+    }
+
+    //create a path with hash
+    string hashPath = commitPath + "/" + commitHash;
+    // create_directory(hashPath);
+
+    //move add folder to commit hash
+    if (!exists(addPath))
+    {
+        cout << "No changes to commit.\n";
+        return;
+    }
+    cout << addPath << "\n"
+         << hashPath << "\n";
+
+    const auto copyOptions = copy_options::recursive;
+    create_directory(hashPath);
+
+    //copy directory to hashpath
+    fs::copy(addPath, hashPath, copyOptions);
+
+    //remove add and addlog
+    remove_all(addPath);
+    fs::remove(addLogPath);
+
+    //update commit log
+    //check if first commit or not by checking commit.log
+    string line;
+    ifstream commitFile(commitLogPath);
+    getline(commitFile, line, ' ');
+    commitFile.close();
+    cout << "line is:" << line << "\n";
+    if (!line.empty())
+    {
+        //get previous addlog path
+        string prevHash = line;
+        getPreviousCommit(prevHash, commitHash);
+    }
+    //update commitlog
+    updateCommitLog(commitHash, message);
+}
+
 int main(int argc, char *argv[])
 {
     root = getenv("dir");
+    vector<string> Args;
+    for (int i = 2; i < argc; i++)
+    {
+        Args.push_back(argv[i]);
+    }
 
     if (argc == 1)
     {
@@ -272,12 +435,13 @@ int main(int argc, char *argv[])
     else if (!strcmp(argv[1], "add"))
     {
         // cout<<"Add function invoked."<<"\n";
-        vector<string> Args;
-        for (int i = 2; i < argc; i++)
-        {
-            Args.push_back(argv[i]);
-        }
+
         add(Args);
+    }
+    else if (!strcmp(argv[1], "commit"))
+    {
+
+        commit(Args);
     }
     return 0;
 }
