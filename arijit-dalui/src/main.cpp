c9bdbb9559a29c8d92cd2f7dd7ed1ef23cfc42c0
arijit-dalui <dalui.arijit11@gmail.com>
2020-10-09 17:29:09 +0000

    Added status+checkout+revert

--- arijit-dalui/src/main.cpp
+++ arijit-dalui/src/main.cpp
@@ -142,6 +142,38 @@ bool tobeignored(std::string path,int onlyImperiumIgnore=0)
 		return true;
 	return false;
 }
+void checkout(string commitHash){
+	string commitPath = root + "/.imperium/commit.log";
+	ifstream commitLog(commitPath.c_str());
+	bool commitExists=0;
+	if(commitLog.is_open()){
+		while(!commitLog.eof()){
+			string line; getline(commitLog,line);
+			int index=-1;
+			while(line[++index]!=' ');
+			line=line.substr(0,index);
+			if(line==commitHash){ commitExists=1; break;}
+		}
+	}
+	else {cout<<"Could not open commit log\n"; return;}
+	commitLog.close();
+	if(commitExists){
+		commitPath=root+"/.imperium/.commit/"+commitHash+"/";
+		for(auto filePath:fsnew::recursive_directory_iterator(commitPath)){
+			struct stat filebuffer;
+			if(stat(filePath.path().c_str(),&filebuffer)==0){
+				if(filebuffer.st_mode & S_IFDIR){
+					addCheckout(filePath.path(),'d');
+				}
+				else if(filebuffer.st_mode & S_IFREG){
+					addCheckout(filePath.path(),'f');
+				}
+			}
+		}
+		cout<<"Checkout successful\n";
+	}
+	else cout<<"No commits found\n";
+}
 	void add(char **argv)
 	{
 		struct stat buffer;
@@ -360,6 +392,262 @@ void commit(std::string message)
 	remove((root+"/.imperium/add.log").c_str());
 	updatecommitlog(commitHash,message);
 }
+void status(){
+	vector<string> staged[3]; vector<string> unstaged[3];
+	string commitHash=getHead();
+	string addLogPath = root + "/.imperium/add.log";
+	ifstream addLog;
+	addLog.open(addLogPath.c_str());
+	if(addLog.is_open()){
+		while(!addLog.eof()){
+			string filePath;
+			getline(addLog,filePath);
+			if(filePath.length()<4) continue;
+			filePath.erase(filePath.begin());
+			filePath.erase(filePath.begin()+filePath.length()-3,filePath.end());
+			filePath=filePath.substr(root.length());
+			string commitPath = root + "/.imperium/.commit/";
+				struct stat buffer;
+				if(stat((commitPath+commitHash+filePath).c_str(),&buffer)==0){
+					string path1=root + "/.imperium/.add"+filePath;
+					string path2=commitPath+commitHash+filePath;
+					string path3= root + filePath;
+					if(comparefiles(path1,path2)){
+						string pushin = root+filePath;
+						staged[1].pb(pushin);
+					}
+					else {
+						string pushin= root+filePath;
+						staged[2].pb(pushin);
+					}
+					if(comparefiles(path1,path3)){
+						string pushin=root+filePath;
+						unstaged[1].pb(pushin);
+					}
+				}
+				else{
+					string pushin=root+filePath;
+					staged[0].pb(pushin);
+				}
+		}
+		string commitPath = root + "/.imperium/.commit/";
+		struct stat commitBuffer; 
+		if(stat(commitPath.c_str(),&commitBuffer)==0){
+			for(auto p:fsnew::recursive_directory_iterator(root)){
+				if(toBeIgnored(p.path(),1)) continue;
+				string commitPath=p.path();
+				if(commitPath.length()<=root.length()) continue;
+				commitPath=commitPath.substr(root.length());
+				string relpath=commitPath;
+				commitPath=root+"/.imperium/.commit/"+commitHash+commitPath;
+				struct stat commitPathBuffer;
+				bool commitPathExists = (stat(commitPath.c_str(),&commitPathBuffer)==0);
+				if(!commitPathExists&&find(staged[0].begin(),staged[0].end(),root+relpath)==staged[0].end())
+				{
+					unstaged[0].pb(root+relpath);
+				}
+				else{
+					if(commitPathExists){
+						if(comparefiles(root+relpath,commitHash)) unstaged[1].pb(root+relpath);
+						else if(find(unstaged[1].begin(),unstaged[1].end(),root+relpath)==unstaged[1].end()) unstaged[2].pb(root+relpath);
+					}
+				}
+			}
+		}
+	}
+	else {cout<<"Could not open add log\n"; return;}
+	addLog.close();
+	cout<<"Staged : \n";
+	cout<<"Created files : \n\n";
+	if(staged[0].empty()) cout<<"Empty\n";
+	for(string files:staged[0]) cout<<"---> "<<files<<endl;
+	cout<<"\nModified Files : \n\n";
+	if(staged[1].empty()) cout<<"Empty\n";
+	for(string files:staged[1]) cout<<"---> "<<files<<endl;
+	cout<<"\nUnchanged Files : \n\n";
+	if(staged[2].empty()) cout<<"Empty\n";
+	for(string files:staged[2]) cout<<"---> "<<files<<endl;
+	cout<<"\nUnstaged : \n";
+	cout<<"Created files : \n\n";
+	if(unstaged[0].empty()) cout<<"Empty\n";
+	for(string files:unstaged[0]) cout<<"---> "<<files<<endl;
+	cout<<"\nModified Files : \n\n";
+	if(unstaged[1].empty()) cout<<"Empty\n";
+	for(string files:unstaged[1]) cout<<"---> "<<files<<endl;
+	cout<<"\nUnchanged Files : \n\n";
+	if(unstaged[2].empty()) cout<<"Empty\n";
+	for(string files:unstaged[2]) cout<<"---> "<<files<<endl;
+	cout<<endl;
+}
+void revert(string passedHash){
+	string headHash = getHead();
+	vector<string> conflict;
+	if(headHash=="ncf") {cout<<"No commits found\n"; return;}
+	for(auto &p:fsnew::recursive_directory_iterator(root)){
+		string path=p.path();
+		struct stat buffer;
+		if(stat(path.c_str(),&buffer)==0){
+			if(buffer.st_mode & S_IFREG){
+				string filerel = path.substr(root.length());
+				string checkPath = root + "/.imperium/.commit/"+headHash+filerel;
+				struct stat buffer1;
+				if(stat(checkPath.c_str(),&buffer1)!=0) continue;
+				if(comparefiles(path,checkPath)) {cout<<"please commit latest changes first\nFile "<<path<<" is modified and unstaged\n"; return;}
+			}
+		}
+	}
+	string lastHash; 
+	ifstream commitLog;
+	commitLog.open(root+"/.imperium/commit.log");
+	bool flag=0;
+	if(commitLog.is_open()){
+		while(!commitLog.eof()){
+			string temp;
+			getline(commitLog,temp);
+			if(temp.size()<20) continue;
+			lastHash=temp;
+			int index=-1; while(lastHash[++index]!=' ');
+			lastHash = lastHash.substr(0,index);
+			if(flag){ break;}
+			if(lastHash==passedHash) flag=1;
+		}
+	}
+	else {cout<<"Could not open commit log\n"; return;}
+	commitLog.close();
+	if(flag == 0) {cout<<"Invalid hash passed\n"; return;}
+	if(lastHash==passedHash) {
+		for(auto &p:fsnew::recursive_directory_iterator(string(root + "/.imperium/.commit/"+passedHash))){
+			string path = p.path();
+			struct stat buffer; string parent=root + "/.imperium/.commit/"+passedHash;
+			string filerel = path.substr(parent.length());
+			if(stat(path.c_str(),&buffer)==0) {
+				if(buffer.st_mode & S_IFREG){
+					string checkPath = root + "/.imperium/.commit/"+headHash+filerel;
+					if(comparefiles(path,checkPath)){
+						ofstream mergeConflict;
+						mergeConflict.open(root+filerel,std::ios_base::app);
+						mergeConflict<<"This file was created in the reverted commit and subsequent changes have been preserved\n";
+						conflict.pb(root+filerel);
+						mergeConflict.close();
+					}
+					else{
+						string deletePath = root + filerel;
+						if(remove(deletePath.c_str())) {cout<<"Error in deleting files\n"; return;}
+					}
+				}
+				else if(buffer.st_mode & S_IFDIR) addCheckout(path,'d');
+			}
+		}
+	}
+	else if(passedHash==headHash){
+		for(auto &p:fsnew::recursive_directory_iterator(string(root+"/.imperium/.commit/"+lastHash)))
+		{
+			string path = p.path();
+			struct stat buffer;
+			if(stat(path.c_str(),&buffer)==0){
+				if(buffer.st_mode & S_IFDIR){
+					addCheckout(path,'d');
+				}
+				else if(buffer.st_mode & S_IFREG){
+					addCheckout(path,'f');
+				}
+			}
+		}
+		for(auto &p:fsnew::recursive_directory_iterator(root)){
+			string path = p.path();
+			string filerel = path.substr(root.length());
+			struct stat buffer; 
+			if(stat(path.c_str(),&buffer)==0){
+				if(buffer.st_mode & S_IFDIR) continue;
+			}
+			string path1 = root + "/.imperium/.commit/"+lastHash+filerel;
+			string path2 = root + "/.imperium/.commit/"+headHash+filerel;
+			struct stat buffer1;
+			if(stat(path1.c_str(),&buffer1)!=0){
+				struct stat buffer2;
+				if(stat(path2.c_str(),&buffer2)==0) remove(path.c_str());
+			}
+		}
+	}
+	else{
+		for(auto &p:fsnew::recursive_directory_iterator(string(root+"/.imperium/.commit/"+lastHash)))
+		{
+			string path = p.path();
+			string parent = root+"/.imperium/.commit/"+lastHash;
+			string filerel =path.substr(parent.length());
+			struct stat buffer;
+			if(stat(path.c_str(),&buffer)==0){
+				if(buffer.st_mode & S_IFREG) {
+					string checkPath = root + "/.imperium/.commit/"+headHash+filerel;
+					struct stat buffer2;
+					if(stat(checkPath.c_str(),&buffer2)==0){
+						if(comparefiles(path,checkPath)){
+							ofstream mergeConflict;
+							mergeConflict.open(root+filerel,std::ios_base::app);
+							mergeConflict<<"Merge conflict, current file has been updated. Preserving changes.\n";
+							conflict.pb(root+filerel);
+							mergeConflict.close();
+						}
+						else addCheckout(path,'f');
+					}
+					else addCheckout(path,'f');
+				}
+			}
+		}
+		for(auto &p:fsnew::recursive_directory_iterator(string(root+"/.imperium/.commit/"+passedHash))){
+			string path = p.path();
+			string parent  = root+"/.imperium/.commit/"+passedHash;
+			string filerel = path.substr(parent.length());
+			struct stat buffer;
+			if(stat(path.c_str(),&buffer)==0){
+				if(buffer.st_mode & S_IFREG){
+					string checkPath = root + "/.imperium/.commit/"+headHash+filerel;
+					struct stat buffer2;
+					if(stat(checkPath.c_str(),&buffer2)==0){
+						if(comparefiles(path,checkPath)){
+							checkPath=root + "/.imperium/.commit/"+lastHash+filerel;
+							struct stat buffer3;
+							if(stat(checkPath.c_str(),&buffer3)!=0)
+							{
+								ofstream mergeConflict;
+								mergeConflict.open(root+filerel,std::ios_base::app);
+								mergeConflict<<"This file was created in the reverted commit and subsequent changes have been preserved\n";
+								conflict.pb(root+filerel);
+								mergeConflict.close();
+							}
+						}
+						else remove(string((root+filerel)).c_str());
+					}
+				}
+				else remove(string((root+filerel)).c_str());
+			}
+		}
+	}
+	if(conflict.size()>0){
+		cout<<"Merge conflicts found with : \n";
+		string conflictPath = root + "/.imperium/conflict.log";
+		remove(conflictPath.c_str());
+		ofstream conflictLog(conflictPath.c_str());
+		conflictLog<<"True\n";
+		for(string s:conflict){
+			cout<<s<<endl;
+			conflictLog<<s<<"\n";
+		}
+		conflict.clear();
+		conflictLog.close();
+	}
+}
+bool conflict(){
+	string conflictPath=root+"/.imperium/conflict.log";
+	ifstream conflictLog;
+	conflictLog.open(conflictPath);
+	if(conflictLog.is_open()){
+		string line;
+		if(conflictLog.eof()) {cout<<"Conflict log empty\n"; exit(0);}
+		else getline(conflictLog,line);
+		return (line=="True");
+	}
+}
     
 int main(int argc,char **argv)
 {
@@ -379,7 +667,24 @@ int main(int argc,char **argv)
 	}
 	else if(strcmp(argv[1],"log")==0)
 	{
-		getcommitlog;
+		getcommitlog();
+	}
+	else if(strcmp(argv[1],"status")==0)
+	{
+		status();
+	}
+	 else if (strcmp(argv[1], "checkout") == 0){
+        if (argc == 3){
+            checkout(argv[2]);
+        }
+        else{
+            cout << "ERROR. Please enter a proper commit hash.\n";
+        }
+    }
+	else if(strcmp(argv[1],"revert")==0){
+		if(argc<3) {cout<<"Please enter a revert hash\n"; return 0;}
+		string revertHash=argv[2]; 
+		revert(revertHash);
 	}
 	std::cout<<"SUCCESS"<<std::endl;
 	return 0;
