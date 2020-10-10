174310f1c61cea29f6152f13a23e365e9ae8769f
koustav-dhar <koustavdhar.31.05@gmail.com>
2020-10-11 03:30:27 +0530

    added revert() function

--- koustav-dhar/src/main.cpp
+++ koustav-dhar/src/main.cpp
@@ -565,67 +565,291 @@ void status()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// IMPERIUM REVERT FUNCTION
+
+void revert(string revertCommitHash);
+
+int commitCheck(string checkPath)
+{
+    string commitFolderPath = root + "/.imperium/.commit/" + checkPath;
+    for (auto &childdir : fs::recursive_directory_iterator(commitFolderPath)){
+        string filename = getCurrentPath(childdir.path(), root + "/.imperium/.commit/" + checkPath);
+        if (checkDirType(childdir.path()) != "\0" && checkDirType(root + filename) != "\0"){
+            if (checkDirType(childdir.path()) == "directory" && checkDirType(root + filename) == "directory")
+                continue;
+            else if (checkDirType(childdir.path()) == "file" && checkDirType(root + filename) == "file"){
+                if (compareFiles(childdir.path(), root + filename))
+                    return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int conflictCheck()
+{
+    ifstream fin;
+    string line;
+    fin.open((root + "/.imperium/conflict").c_str());
+    getline(fin, line);
+    fin.close();
+    if (line == "true")
+        return 1;
+    return 0;
+}
+
+void resolve()
+{
+    ofstream fout;
+    fout.open((root + "/.imperium/conflict").c_str(), ofstream::trunc);
+    fout << "false\n";
+    fout.close();
+}
+
+void mergeConflict()
+{
+    ofstream fout;
+    fout.open((root + "/.imperium/conflict").c_str(), ofstream::trunc);
+    fout << "true\n";
+    fout.close();
+}
+
+void revert(string revertCommitHash)
+{
+    string commitFolderName = revertCommitHash.substr(0, 40);
+    bool switchBool = false , dirBool = false;
+    ifstream commitLog((root + "/.imperium/commit.log").c_str());
+    string line;
+    string lastCommit = "";
+    if (getline(commitLog, line)){
+        lastCommit = line.substr(7, 40);
+    }
+    commitLog.close();
+    commitLog.open((root + "/.imperium/commit.log").c_str());
+    while (getline(commitLog, line)){
+        if (switchBool == true){
+            commitFolderName = line.substr(7, 40);
+            switchBool = false;
+            break;
+        }
+        if (commitFolderName == line.substr(7, 40)){
+            switchBool = true;
+            dirBool = true;
+        }
+    }
+    commitLog.close();
+
+    if (!dirBool){
+        cout << "ERROR. Invalid Hash provided.\n";
+        return;
+    }
+    else if (dirBool && lastCommit != ""){
+        if (commitCheck(lastCommit) == 0){
+            if (!switchBool){
+                string passedHash = revertCommitHash.substr(0, 40);
+                string commitFolderPath = root + "/.imperium/.commit/" + commitFolderName;
+                if (lastCommit == passedHash){
+                    fs::copy(commitFolderPath, root + "/", fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+                    for (auto &childdir : fs::recursive_directory_iterator(root)){
+                        if (checkDirType(childdir.path()) != "\0"){
+                            string relPath = getCurrentPath(childdir.path());
+                            string prevPath = commitFolderPath + relPath;
+                            string revertPath = root + "/.imperium/.commit/" + passedHash + relPath;
+                            if (checkDirType(childdir.path()) == "directory")
+                                continue;
+                            else if (checkDirType(childdir.path()) == "file"){
+                                if (checkDirType(prevPath) == "\0" && checkDirType(revertPath) != "\0"){
+                                    remove(childdir.path());
+                                }
+                            }
+                        }
+                    }
+                }
+                else{
+                    for (auto &childdir : fs::recursive_directory_iterator(commitFolderPath)){
+                        if (checkDirType(childdir.path()) != "\0"){
+                            if (checkDirType(childdir.path()) == "directory")
+                                continue;
+                            else if (checkDirType(childdir.path()) == "file"){
+                                string relPath = getCurrentPath(childdir.path());
+                                string rootPath = root + "/" + relPath;
+                                string revertPath = root + "/.imperium/.commit/" + passedHash + relPath;
+                                if (checkDirType(rootPath) != "\0"){
+                                    if (compareFiles(rootPath, childdir.path())){
+                                        ifstream fin(childdir.path().c_str());
+                                        ofstream fout(rootPath, ofstream::out | ofstream::app);
+                                        fout << "\n \nYOUR CHANGES__________<<<<<<<<<<<<<<<<<<<<<";
+                                        fout << "\n \n______INCOMING CHANGES>>>>>>>>>>>>>>>>>>>>>\n \n";
+                                        fout << fin.rdbuf();
+                                        cout << "MERGE CONFLICT IN : " << rootPath << endl;
+                                    }
+                                    else{
+                                        continue;
+                                    }
+                                }
+                                else{
+                                    fs::copy_file(childdir.path(), rootPath, fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+                                }
+                            }
+                        }
+                    }
+                    for (auto &childdir : fs::recursive_directory_iterator(root + "/.imperium/.commit/" + passedHash)){
+                        string relPath = getCurrentPath(childdir.path(), root + "/.imperium/.commit/" + passedHash);
+                        if (checkDirType(childdir.path()) != "\0"){
+                            if (checkDirType(childdir.path()) == "directory")
+                                continue;
+                            else if (checkDirType(childdir.path()) == "file"){
+                                string rootPath = root + relPath;
+                                if (checkDirType(rootPath) != "\0"){
+                                    if (compareFiles(rootPath, childdir.path())){
+                                        ofstream fout(rootPath, ofstream::out | ofstream::app);
+                                        fout << "\n\nTHIS FILE WAS CREATED IN THE REVERTED COMMIT,\n";
+                                        fout << "HOWEVER YOUR CHANGES IN THE SUCCESSING COMMITS ARE SAVED HERE\n\n";
+                                        cout << "MERGE CONFLICT IN : " << rootPath << endl;
+                                    }
+                                    else{
+                                        remove(rootPath.c_str());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else{
+                string commitFolderPath = root + "/.imperium/.commit/" + commitFolderName;
+                for (auto &childdir : fs::recursive_directory_iterator(commitFolderPath)){
+                    string relPath = getCurrentPath(childdir.path(), root + "/.imperium/.commit/" + commitFolderName);
+                    if (checkDirType(childdir.path()) != "\0"){
+                        if (checkDirType(childdir.path()) == "directory")
+                            continue;
+                        else if (checkDirType(childdir.path()) == "file"){
+                            string rootPath = root + relPath;
+                            string prevPath = commitFolderPath + relPath;
+                            if (checkDirType(rootPath) != "\0" && checkDirType(prevPath) == "\0"){
+                                if (compareFiles(rootPath, childdir.path())){
+                                    ofstream fout(rootPath, ofstream::out | ofstream::app);
+                                    fout << "\n\nTHIS FILE WAS NOT PRESENT IN THE PREVIOUS CHECKPOINT,\n";
+                                    fout << "HOWEVER YOUR CHANGES IN THE SUCCESSING COMMITS ARE SAVED HERE\n\n";
+                                    cout << "MERGE CONFLICT IN : " << rootPath << endl;
+                                }
+                                else{
+                                    remove(rootPath.c_str());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            mergeConflict();
+        }
+        else{
+            cout << "You still have changes that need to be commited.\n";
+        }
+    }
+    else{
+        cout << "ERROR. Invalid Hash provided.\n";
+        return;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void getHelp()
+{
+    cout << "IMPERIUM COMMANDS: \n";
+    cout << "imperium - Get the Commands List.\n";
+    cout << "imperium init - Initialize an empty repository.\n";
+    cout << "imperium add <path> - Add directories or files to repository. If you want to set the path as root, just type \"imperium add .\"\n";
+    cout << "imperium commit \"commit message\" - Commit the currently staged files.\n";
+    cout << "imperium log - See the commit log until now.\n";
+    cout << "imperium status - See the current status of files which are created/modified as staged or unstaged for commit.\n";
+    cout << "imperium checkout <commitHashID> - Checkout the files committed in the required commit with given commitHash to the root.\n";
+    cout << "imperium revert <commitHashID> - Revert the commit with given commitHash.\n";
+    cout << "imperium resolve - Resolve Merge Conflicts and make the system ready for making commits.\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char** argv) {
 
     const string dir = getenv("dir");
     root = dir;
 
-    if (strcmp(argv[1], "init") == 0) {
-        if (argc == 2) {
-            init(root);
-        }
-        else {
-            cout << "ERROR. Command arguments isn't valid.\n";
-            cout << "Did you mean the command \"imperium init\".\n";
-        }
+    if (argc == 1){
+        getHelp();
     }
-    else if (strcmp(argv[1], "add") == 0) {
-        if (argc == 3) {
-            string path = root;
-            path += "/";
-            if (strcmp(argv[2], ".") != 0)
-                path += argv[2];
-            add(path);
-        }
-        else {
-            cout << "Please specify a proper path.\n";
-        }
+    else if (strcmp(argv[1], "resolve") == 0){
+        resolve();
     }
-    else if (strcmp(argv[1], "commit") == 0){
-        if (argc < 3 || argv[2] == ""){
-            cout << "Please add a proper commit message.\n";
-        }
-        else{
-            string message = "";
-            for (int i = 2; i < argc; ++i){
-                message += argv[i];
-                if (i != argc - 1)
-                    message += " ";
+    else if (conflictCheck() == 0){
+        if (strcmp(argv[1], "init") == 0) {
+            if (argc == 2) {
+                init(root);
             }
-            commit(message);
+            else {
+                cout << "ERROR. Command arguments isn't valid.\n";
+                cout << "Did you mean the command \"imperium init\".\n";
+            }
+        }
+        else if (strcmp(argv[1], "add") == 0) {
+            if (argc == 3) {
+                string path = root;
+                path += "/";
+                if (strcmp(argv[2], ".") != 0)
+                    path += argv[2];
+                add(path);
+            }
+            else {
+                cout << "Please specify a proper path.\n";
+            }
+        }
+        else if (strcmp(argv[1], "commit") == 0){
+            if (argc < 3 || argv[2] == ""){
+                cout << "Please add a proper commit message.\n";
+            }
+            else{
+                string message = "";
+                for (int i = 2; i < argc; ++i){
+                    message += argv[i];
+                    if (i != argc - 1)
+                        message += " ";
+                }
+                commit(message);
+            }
+        }
+        else if (strcmp(argv[1], "log") == 0){
+            if (argc == 2){
+                getCommitLog();
+            }
+            else{
+                cout << "ERROR. Command arguments isn't valid.\n";
+                cout << "Did you mean the command \"imperium log\".\n";   
+            }
+        }
+        else if (strcmp(argv[1], "checkout") == 0){
+            if (argc == 3){
+                checkout(argv[2]);
+            }
+            else{
+                cout << "ERROR. Please enter a proper commit hash.\n";
+            }
+        }
+        else if (strcmp(argv[1], "status") == 0){
+            status();
+        }
+        else if (strcmp(argv[1], "revert") == 0){
+            if (argc == 3){
+                revert(argv[2]);
+            }
+            else{
+                cout << "ERROR. Please enter a proper commit hash.\n";
+            }
         }
     }
-    else if (strcmp(argv[1], "log") == 0){
-        if (argc == 2){
-            getCommitLog();
-        }
-        else{
-            cout << "ERROR. Command arguments isn't valid.\n";
-            cout << "Did you mean the command \"imperium log\".\n";   
-        }
+    else{
+        cout << "You have to make a commit, after resolving any conflicts, if there, type \"imperium resolve\"\n";
     }
-    else if (strcmp(argv[1], "checkout") == 0){
-        if (argc == 3){
-            checkout(argv[2]);
-        }
-        else{
-            cout << "ERROR. Please enter a proper commit hash.\n";
-        }
-    }
-    else if (strcmp(argv[1], "status") == 0){
-        status();
-    }
-
     return 0;
 
 }
