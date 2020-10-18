fa88273637cdef28090744dbdf78a28a3e87ae49
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-10-18 12:03:17 +0530

    PR - Final upto revert

--- src/main.cpp
+++ src/main.cpp
@@ -486,22 +486,534 @@ void commit(char **argv)
     }
 }
 
-int main(int argc, const char **argv)
+int BUFFER_SIZE = 8192;
+
+int compareFiles(std::string path1, std::string path2)
+{
+
+    std::ifstream lFile(path1.c_str(), std::ifstream::in | std::ifstream::binary);
+    std::ifstream rFile(path2.c_str(), std::ifstream::in | std::ifstream::binary);
+
+    if (!lFile.is_open() || !rFile.is_open())
+    {
+        return 1;
+    }
+
+    char *lBuffer = new char[BUFFER_SIZE]();
+    char *rBuffer = new char[BUFFER_SIZE]();
+
+    do
+    {
+        lFile.read(lBuffer, BUFFER_SIZE);
+        rFile.read(rBuffer, BUFFER_SIZE);
+        int numberOfRead = lFile.gcount(); //I check the files with the same size
+        if (numberOfRead != rFile.gcount())
+            return 1;
+
+        if (std::memcmp(lBuffer, rBuffer, numberOfRead) != 0)
+        {
+            memset(lBuffer, 0, numberOfRead);
+            memset(rBuffer, 0, numberOfRead);
+            return 1;
+        }
+    } while (lFile.good() || rFile.good());
+
+    delete[] lBuffer;
+    delete[] rBuffer;
+
+    return 0;
+}
+
+int commitCheck(std::string checkPath)
+{
+    std::string commitFolderPath = root + "/.imperium/.commit/" + checkPath;
+    for (auto &p : fs::recursive_directory_iterator(commitFolderPath))
+    {
+        std::string filename = ((std::string)p.path()).substr(root.length() + 19 + checkPath.length());
+        struct stat s;
+        struct stat b;
+        if (stat(p.path().c_str(), &s) == 0 && stat((root + filename).c_str(), &b) == 0)
+        {
+
+            if (s.st_mode & S_IFDIR && b.st_mode & S_IFDIR)
+                continue;
+            else if (s.st_mode & S_IFREG && b.st_mode & S_IFREG)
+            {
+                if (compareFiles(p.path(), (root + filename)))
+                    return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void mergeConflict()
+{
+    std::ofstream cfile;
+    cfile.open(root + "/.imperium/conflict", std::ofstream::trunc);
+    cfile << "true\n";
+    cfile.close();
+}
+
+void revert(char **argv)
+{
+    if (argv[2] == "")
+    {
+        std::cout << "Please provide a Hash"
+                  << "\n";
+        return;
+    }
+    std::string revertCommitHash = argv[2];
+    std::string commitFolderName = revertCommitHash.substr(0, 40);
+    bool switchBool = false;
+    bool dirBool = false;
+    std::ifstream commitLog;
+    std::string commitLine;
+
+    commitLog.open(root + "/.imperium/commit.log");
+    std::string lastCommit = "";
+    if (std::getline(commitLog, commitLine))
+    {
+        lastCommit = commitLine.substr(0, 40);
+    }
+    commitLog.close();
+
+    commitLog.open(root + "/.imperium/commit.log");
+    while (std::getline(commitLog, commitLine))
+    {
+        if (switchBool == true)
+        {
+            commitFolderName = commitLine.substr(0, 40);
+            switchBool = false;
+            break;
+        }
+
+        //std::cout << commitLine.substr(0, 40) << "\n";
+        if (commitFolderName == commitLine.substr(0, 40))
+        {
+
+            switchBool = true;
+            dirBool = true;
+        }
+    }
+    commitLog.close();
+
+    if (!dirBool)
+    {
+        std::cout << "incorrect Hash provided"
+                  << "\n";
+        return;
+    }
+    else if (dirBool && lastCommit != "")
+    {
+        if (commitCheck(lastCommit) == 0)
+        {
+            if (!switchBool)
+            {
+                std::string passedHash = revertCommitHash.substr(0, 40);
+                std::string commitFolderPath = root + "/.imperium/.commit/" + commitFolderName;
+                if (lastCommit == passedHash)
+                {
+                    fs::copy(commitFolderPath, root + "/", fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+                    for (auto &p : fs::recursive_directory_iterator(root))
+                    {
+                        struct stat s;
+
+                        if (stat(p.path().c_str(), &s) == 0)
+                        {
+                            std::string relPath = ((std::string)p.path()).substr(root.length());
+                            std::string prevPath = commitFolderPath + relPath;
+                            std::string revertPath = root + "/.imperium/.commit/" + passedHash + relPath;
+
+                            struct stat a;
+                            struct stat b;
+                            if (s.st_mode & S_IFDIR)
+                                continue;
+                            else if (s.st_mode & S_IFREG)
+                            {
+                                if (stat(prevPath.c_str(), &a) != 0 && stat(revertPath.c_str(), &b) == 0)
+                                {
+                                    remove(p.path());
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (auto &p : fs::recursive_directory_iterator(commitFolderPath))
+                    {
+                        struct stat s;
+
+                        if (stat(p.path().c_str(), &s) == 0)
+                        {
+                            if (s.st_mode & S_IFDIR)
+                                continue;
+                            else if (s.st_mode & S_IFREG)
+                            {
+                                std::string relPath = ((std::string)p.path()).substr(commitFolderPath.length());
+                                std::string rootPath = root + relPath;
+                                std::string revertPath = root + "/.imperium/.commit/" + passedHash + relPath;
+                                struct stat a;
+                                struct stat b;
+                                if (stat(rootPath.c_str(), &a) == 0)
+                                {
+                                    if (compareFiles(rootPath, p.path()))
+                                    {
+                                        std::ifstream ifile(p.path().c_str(), std::ifstream::in);
+                                        std::ofstream ofile(rootPath, std::ofstream::out | std::ofstream::app);
+                                        ofile << "\n \nYOUR CHANGES________<<<<<<<<<<<<<<<<<<<\n \n_________INCOMING CHANGES>>>>>>>>>>>>>>>>\n \n";
+                                        ofile << ifile.rdbuf();
+                                        std::cout << "\033[1;31mMERGE CONFLICT IN : \033[0m" << rootPath << "\n";
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                                else
+                                {
+                                    fs::copy_file(p.path(), rootPath, fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+                                }
+                            }
+                        }
+                    }
+
+                    for (auto &p : fs::recursive_directory_iterator(root + "/.imperium/.commit/" + passedHash))
+                    {
+                        std::string relPath = ((std::string)p.path()).substr((root + "/.imperium/.commit/" + passedHash).length());
+                        struct stat s;
+                        if (stat(p.path().c_str(), &s) == 0)
+                        {
+                            if (s.st_mode & S_IFDIR)
+                                continue;
+                            else if (s.st_mode & S_IFREG)
+                            {
+                                struct stat a;
+                                struct stat b;
+                                std::string rootPath = root + relPath;
+                                std::string prevPath = commitFolderPath + relPath;
+                                if (stat(rootPath.c_str(), &a) == 0 && stat(prevPath.c_str(), &b) != 0)
+                                {
+                                    if (compareFiles(rootPath, p.path()) == 0)
+                                        remove(rootPath.c_str());
+                                    else
+                                    {
+                                        std::ofstream ofile(rootPath, std::ofstream::out | std::ofstream::app);
+                                        ofile << "\n\nTHIS FILE WASN'T PRESENT IN THE previous checkpoint,\nhowever your changes in the succeeesing commits are saved here\n\n";
+                                        std::cout << "\033[1;31mMERGE CONFLICT IN : \033[0m" << rootPath << "\n";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                std::string commitFolderPath = root + "/.imperium/.commit/" + commitFolderName;
+                for (auto &p : fs::recursive_directory_iterator(commitFolderPath))
+                {
+                    struct stat s;
+                    std::string relPath = ((std::string)p.path()).substr((root + "/.imperium/.commit/" + commitFolderName).length());
+                    if (stat(p.path().c_str(), &s) == 0)
+                    {
+                        if (s.st_mode & S_IFDIR)
+                            continue;
+                        else if (s.st_mode & S_IFREG)
+                        {
+                            struct stat a;
+                            std::string rootPath = root + relPath;
+                            if (stat(rootPath.c_str(), &a) == 0)
+                            {
+                                if (compareFiles(rootPath, p.path()) == 0)
+                                    remove(rootPath.c_str());
+                                else
+                                {
+                                    std::ofstream ofile(rootPath, std::ofstream::out | std::ofstream::app);
+                                    ofile << "\n\nTHIS FILE WAS CREATED IN THE REVERTED COMMIT,\nhowever your changes in the succeeesing commits are saved here\n\n";
+                                    std::cout << "\033[1;31mMERGE CONFLICT IN : \033[0m" << rootPath << "\n";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            mergeConflict();
+        }
+        else
+        {
+            std::cout << "\033[1;31mYou've changes that need to be commited or stashed.\033[0m\n";
+        }
+    }
+    else
+    {
+        std::cout << "Incorrect Hash provided"
+                  << "\n";
+        return;
+    }
+}
+
+int conflictCheck()
+{
+    std::ifstream cfile;
+    std::string line;
+    cfile.open(root + "/.imperium/conflict");
+    std::getline(cfile, line);
+    cfile.close();
+    if (line == "true")
+        return 1;
+    return 0;
+}
+
+void status()
+{
+    struct stat buf;
+    std::ifstream readAddLog;
+    std::string line, headHash;
+    std::vector<std::string> staged;
+    std::vector<std::string> type;
+    std::vector<std::string> notstaged;
+    std::ifstream readCommitLog;
+    std::string stagedPath;
+
+    readCommitLog.open(root + "/.imperium/commit.log");
+    std::getline(readCommitLog, headHash);
+
+    headHash = headHash.substr(0, 40);
+    readCommitLog.close();
+
+    if (stat((root + "/.imperium/.add").c_str(), &buf) == 0)
+    {
+        readAddLog.open(root + "/.imperium/add.log");
+
+        while (std::getline(readAddLog, line))
+        {
+            stagedPath = line.substr(root.length() + 1, line.length() - root.length() - 4);
+            if (stat((root + "/.imperium/.add/" + stagedPath).c_str(), &buf) == 0)
+            {
+                if ((stat((root + "/.imperium/.commit/" + headHash + stagedPath).c_str(), &buf) != 0) || (stat((root + "/.imperium/.commit").c_str(), &buf) != 0))
+                {
+                    type.push_back("created: ");
+                    staged.push_back(stagedPath);
+                }
+                if (std::find(staged.begin(), staged.end(), stagedPath) == staged.end())
+                {
+                    type.push_back("modified: ");
+                    staged.push_back(stagedPath);
+                }
+                if (stat((root + stagedPath).c_str(), &buf) != 0)
+                {
+                    notstaged.push_back("deleted: " + stagedPath);
+                }
+                else
+                {
+                    if (compareFiles(root + stagedPath, root + "/.imperium/.add" + stagedPath))
+                    {
+                        notstaged.push_back("modified: " + stagedPath);
+                    }
+                }
+            }
+        }
+    }
+    readAddLog.close();
+    struct stat s;
+    if (stat((root + "/.imperium/.commit").c_str(), &buf) == 0)
+    {
+        for (auto &p : fs::recursive_directory_iterator(root))
+        {
+            if (stat(p.path().c_str(), &s) == 0)
+            {
+                if (s.st_mode & S_IFREG)
+                {
+                    std::string rootPath = p.path();
+
+                    if (toBeIgnored(p.path().c_str(), 1))
+                        continue;
+
+                    std::string commitPath = root + "/.imperium/.commit/" + headHash + rootPath.substr(root.length());
+
+                    if (stat((commitPath).c_str(), &s) != 0 && (std::find(staged.begin(), staged.end(), rootPath.substr(root.length())) == staged.end()))
+                    {
+                        for (int i = 0; i < staged.size(); i++)
+                        {
+                            std::cout << staged[i] << "\n";
+                        }
+                        notstaged.push_back("created: " + rootPath.substr(root.length()));
+                    }
+                    else
+                    {
+                        if (compareFiles(rootPath, commitPath))
+                        {
+                            if (std::find(staged.begin(), staged.end(), rootPath.substr(root.length())) == staged.end() && std::find(notstaged.begin(), notstaged.end(), stagedPath) == notstaged.end())
+                            {
+                                notstaged.push_back("modified: " + rootPath.substr(root.length()));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        for (auto &p : fs::recursive_directory_iterator(root + "/.imperium/.commit/" + headHash))
+        {
+            struct stat s;
+            if (stat(p.path().c_str(), &s) == 0)
+            {
+                if (s.st_mode & S_IFREG)
+                {
+                    std::string commitPath = p.path();
+                    std::string rootPath = root + commitPath.substr(root.length() + 59);
+
+                    if (toBeIgnored(rootPath.c_str(), 1))
+                        continue;
+
+                    if (stat((rootPath).c_str(), &s) != 0)
+                    {
+                        // if(std::find(notstaged.begin(), notstaged.end(), rootPath.substr(root.length())) != notstaged.end())
+                        notstaged.push_back("deleted: " + rootPath.substr(root.length()));
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+    if (stat((root + "/.imperium/.commit").c_str(), &buf) != 0)
+    {
+        for (auto &p : fs::recursive_directory_iterator(root))
+        {
+            struct stat s;
+            if (stat(p.path().c_str(), &s) == 0)
+            {
+                if (s.st_mode & S_IFREG)
+                {
+                    std::string rootPath = p.path();
+
+                    if (toBeIgnored(rootPath.c_str(), 1))
+                        continue;
+
+                    if (stat((root + "/.imperium/.add").c_str(), &buf) != 0 || (std::find(staged.begin(), staged.end(), rootPath.substr(root.length())) == staged.end()))
+                    {
+                        notstaged.push_back("created: " + rootPath.substr(root.length()));
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+    if (notstaged.size())
+        std::cout << "Changes not staged for commit:\n\n";
+    for (int i = 0; i < notstaged.size(); i++)
+    {
+        std::cout << notstaged[i] << "\n";
+    }
+    if (staged.size())
+        std::cout << "\nChanges staged for commit:\n\n";
+    for (int i = 0; i < staged.size(); i++)
+    {
+        std::cout << type[i] << staged[i] << "\n";
+    }
+    if (!notstaged.size() && !staged.size())
+        std::cout << "Up to date\n";
+    staged.clear();
+    notstaged.clear();
+}
+
+void resolve()
+{
+    std::ofstream cfile;
+    cfile.open(root + "/.imperium/conflict", std::ofstream::trunc);
+    cfile << "false\n";
+    cfile.close();
+}
+
+void checkout(char **argv)
+{
+    if (argv[2] == "")
+    {
+        std::cout << "Please provide a Hash"
+                  << "\n";
+        return;
+    }
+    std::string checkoutCommitHash = argv[2];
+    std::string commitFolderName = checkoutCommitHash.substr(0, 40);
+    bool directoryExists = false;
+    std::ifstream commitLog;
+    std::string commitLine;
+    commitLog.open(root + "/.imperium/commit.log");
+    while (std::getline(commitLog, commitLine))
+    {
+        //std::cout << commitLine.substr(0, 40) << "\n";
+        if (commitFolderName == commitLine.substr(0, 40))
+        {
+            directoryExists = true;
+            break;
+        }
+    }
+    commitLog.close();
+    if (!directoryExists)
+    {
+        std::cout << "Incorrect Hash provided"
+                  << "\n";
+        return;
+    }
+    std::string commitFolderPath = root + "/.imperium/.commit/" + commitFolderName;
+    fs::copy(commitFolderPath, root + "/", fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+}
+
+int main(int argc, char **argv)
 {
     const char *dir = getenv("dir");
     root = dir;
+    if (strcmp(argv[1], "resolve") == 0)
+    {
+        resolve();
+    }
+    else if (conflictCheck() == 0)
+    {
+        if (strcmp(argv[1], "init") == 0)
+        {
+            init(dir);
+        }
+        if (strcmp(argv[1], "add") == 0)
+        {
+            add(argv);
+        }
+        else if (strcmp(argv[1], "log") == 0)
+        {
+            getCommitLog();
+        }
 
-    if (strcmp(argv[1], "init") == 0)
-    {
-        init(dir);
+        else if (strcmp(argv[1], "commit") == 0)
+        {
+            commit(argv);
+        }
+        else if (strcmp(argv[1], "checkout") == 0)
+        {
+            checkout(argv);
+        }
+        else if (strcmp(argv[1], "revert") == 0)
+        {
+            revert(argv);
+        }
+        else if (strcmp(argv[1], "status") == 0)
+        {
+            status();
+        }
     }
-    if (strcmp(argv[1], "add") == 0)
+    else
     {
-        add(argv);
+        std::cout << "\033[1;31mYou've to make a commit, after resolving any conflicts, if there, type 'imperium resolve'\033[0m\n";
     }
-    else if (strcmp(argv[1], "commit") == 0)
-    {
-        commit(argv);
-    }
-    return 0;
 }
